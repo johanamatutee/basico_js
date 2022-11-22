8f1c0cc028f76ca56857a3577d00a97468df5248
johanamatutee <107896323+johanamatutee@users.noreply.github.com>
2022-11-22 11:51:44 -0300

    Update basico_remake.js

--- basico_remake.js
+++ basico_remake.js
@@ -1,1 +1,41 @@
+/* JS es 
+un lenguaje interpretado
+orientado a obj
+debilmente tipado: no requiere indicar el tipo de dato que guarda la variable tanto como JAVA. Java es fuertemente tipado
+dinamico: se compila en el browser, durante el runtime 
+es Backwards compatible: compatible con versiones anteriores pero las nuevas cosas no rompen lo que estan.
+BABEL es un compilador de JS para hacer a JS fordwards compatible
+*/
 
+//Forwards: ser compatible con las funciones nuevas
+
+
+/* para 2019 para las web se utilizaba HTML(para maquetar) CSS(estilos) JS(dinamicidad)
+W3e subió WebAssembly que es la competencia.
+
+JavaScript es mas utilizado y tiene una gran comunidad.
+Tiene muchos frameworks y librerias 
+para productos web:
+Angular
+React
+View
+
+Desarrollo de Apps android y IOS
+React Native
+
+Para apps de escritorio 
+Electron
+
+Back-end y iot
+Node JS 
+*/
+
+
+/*COMPONENTES PRINCIPALES DEL LENGUAJE
+Datos
+Funciones
+
+
+
+
+*/
