7ece00f38ff6143b5b1d55ffb42ea80295acc346
johanamatutee <107896323+johanamatutee@users.noreply.github.com>
2022-11-28 11:33:55 -0300

    TestJuego

--- piedraPapelTijera.js
+++ piedraPapelTijera.js
@@ -3,3 +3,19 @@ var papel = 2;
 var tijera = 3;
 
 
+
+var eleccion ; 
+var cpu = 2;
+
+console.log("Ingrese una opcion: ${piedra} para elegir PIEDRA, ${papel} para elegir PAPEL, ${tijera} para elegir TIJERA");
+InputEvent(eleccion);
+
+if (eleccion == cpu){
+
+} else if (eleccion > cpu) {
+
+} else if (eleccion > cpu){
+
+} else{
+    console.log("No es una opcion valida")
+}
